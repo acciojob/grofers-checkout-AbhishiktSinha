4ad5b417ecd9981841e3e700de6c0648953f4a76
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-12-08 18:26:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,39 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+const table = document.querySelector("table");
+
 const getSum = () => {
 //Add your code here
-  
+
+	// extract all prices and add them
+	let sum = 0;
+	const priceList = document.querySelectorAll(".price");
+
+	for(let price of priceList) {
+		sum += Number(price.textContent);
+	}
+
+	// create or find totalPrice row and cell or add a new row and add a td to it
+	let totalPriceRow;
+	let totalPriceCell;
+	if(document.querySelector(".total-price-row")) {
+		totalPriceRow = document.querySelector(".total-price-row");
+		totalPrceCell = totalPriceRow.querySelector(".total-price-cell");
+	}
+	else {
+		totalPriceRow = document.createElement("tr");
+		totalPriceCell = document.createElement("td");
+		
+		totalPriceRow.className = "total-price-row";
+		totalPriceCell.className = "total-price-cell";
+		// totalPriceCell.setAttribute("colspan", "2");
+		totalPriceRow.appendChild(totalPriceCell);
+
+		table.appendChild(totalPriceRow);
+	}
+
+	totalPriceCell.textContent = `${sum}`;	
 };
 
 getSumBtn.addEventListener("click", getSum);
